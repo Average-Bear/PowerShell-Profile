f80381b12b40ba549a28a8450f0ecab801e39256
Average-Bear <jeremyndewitt@gmail.com>
2017-08-24 08:18:17 +1200

    Update and rename Profile_v2.9.ps1 to Profile_v3.0.2.ps1

--- Profile_v3.0.2.ps1
+++ Profile_v3.0.2.ps1
@@ -1,12 +1,14 @@
-$ProVersion = "v3.0.2"
+ $ProVersion = "v3.0.2"
 <#
 PowerShell Profile $ProVersion
 Author: Jeremy DeWitt aka JBear
+
 Update Notes:
-Version 3.0.2: 
-	-Renamed NewADUser to CreateNewUser. 
-	-Added GUI to CreateNewUser process. 
-Version 3.0.1: 
+Version 3.0.2:
+	-Renamed NewADUser to CreateNewUser.
+	-Added GUI to CreateNewUser process.
+	-Fixed several GUI bugs.	
+Version 3.0.1:
 	-Added PatchSearch function to report specific patches and install times.
 Version 2.9:
 	- Added InstallPackage function to handle .EXE, .MSI, and .MSP installs in a single function.
@@ -339,6 +341,166 @@ function HuntUser {
     Query
 
 }#End HuntUser
+
+function PatchSearch {
+
+<# 
+.SYNOPSIS
+    Reports uptimes of specified workstations.
+
+.EXAMPLE
+    .\Script.ps1 Server01, Server02
+    Reports uptimes for Server01, Server02
+
+.EXAMPLE
+    .\Script.ps1 -FindHotFix KB4019264, KB982018
+    Reports uptimes for all servers in Default array and searched for specified HotFixes. Value(s) will only return if they are found.
+
+.NOTES
+    Written by JBear 5/19/16	
+#>
+
+[cmdletbinding()]
+
+Param (
+
+    [Parameter(Mandatory=$true,position=0)]
+    $ComputerName, #= ((Get-ADComputer -Filter * -SearchBase "OU=05_Servers,DC=ACME,DC=COM").Name),
+
+    #Format today's date
+    $LogDate = (Get-Date -format yyyyMMdd),
+
+    $OutFile = [environment]::getfolderpath("mydocuments") + "\" + $LogDate + "-UpTime\KBReport.csv",
+
+    [Parameter(position=1)]
+    [Switch]$FindHotFix,
+
+    [Parameter(ValueFromPipeline=$true,Position=2)]
+    [String[]]$KB=@()
+)	
+
+$i=0
+$j=0
+			
+    function UptimeReport {
+
+        foreach ($Computer in $ComputerName) {
+
+            Write-Progress -Activity "Retrieving Uptime and HotFix results..." -Status ("Percent Complete:" + "{0:N0}" -f ((($i++) / $ComputerName.count) * 100) + "%") -CurrentOperation "Processing $($Computer)..." -PercentComplete ((($j++) / $ComputerName.count) * 100)
+
+            if(!([String]::IsNullOrWhiteSpace($Computer))) {
+
+                if (Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
+
+                    Start-Job { param($Computer, $ComputerName, $KB)
+
+                        if($KB) {
+
+                            Invoke-Command -ComputerName $Computer {param ($Computer, $KB)
+
+                                $uptime = Get-WmiObject Win32_OperatingSystem -ComputerName $Computer
+                                $bootTime = $uptime.ConvertToDateTime($uptime.LastBootUpTime)
+                                $elapsedTime = (Get-Date) - $bootTime
+                                $HotFixResults = Get-HotFix -Id $KB -ErrorAction SilentlyContinue
+
+                                if($HotFixResults) {
+
+                                    foreach($HotFix in $HotFixResults) {
+
+                                        [pscustomobject] @{
+
+                                            ComputerName = $Computer
+                                            LastBootTime = $bootTime
+                                            ElapsedTime = '{0:00} Days, {1:00} Hours, {2:00} Minutes, {3:00} Seconds' -f $elapsedTime.Days, $elapsedTime.Hours, $elapsedTime.Minutes, $elapsedTime.Seconds
+                                            HotFix=$HotFix.HotFixID
+                                            HFInstallDate=$HotFix.InstalledOn
+                                        }
+                                    }
+
+                                    $KB | ForEach-Object {
+                                            
+                                        if(!($hotfixresults.HotFixID -contains $_)) {
+                                        
+                                            [pscustomobject] @{
+
+                                                ComputerName = $Computer
+                                                LastBootTime = $bootTime
+                                                ElapsedTime = '{0:00} Days, {1:00} Hours, {2:00} Minutes, {3:00} Seconds' -f $elapsedTime.Days, $elapsedTime.Hours, $elapsedTime.Minutes, $elapsedTime.Seconds
+                                                HotFix=$_
+                                                HFInstallDate='Not Installed'
+                                            }
+                                        }
+                                    }
+                                }
+
+                                else {
+
+                                    $KB | ForEach-Object {
+                                            
+                                        if(!($hotfixresults.HotFixID -contains $_)) {
+
+                                            [pscustomobject] @{
+
+                                                ComputerName = $Computer
+                                                LastBootTime = $bootTime
+                                                ElapsedTime = '{0:00} Days, {1:00} Hours, {2:00} Minutes, {3:00} Seconds' -f $elapsedTime.Days, $elapsedTime.Hours, $elapsedTime.Minutes, $elapsedTime.Seconds
+                                                HotFix=$_
+                                                HFInstallDate='Not Installed'
+                                            }    
+                                        }
+                                    }                                                  
+                                }
+                            } -ArgumentList $Computer, $KB
+                        }
+
+                        else {
+
+                            $uptime = Get-WmiObject Win32_OperatingSystem -ComputerName $Computer
+                            $bootTime = $uptime.ConvertToDateTime($uptime.LastBootUpTime)
+                            $elapsedTime = (Get-Date) - $bootTime
+
+                            [pscustomobject] @{
+
+                                ComputerName = $Computer
+                                LastBootTime = $bootTime
+                                ElapsedTime = '{0:00} Days, {1:00} Hours, {2:00} Minutes, {3:00} Seconds' -f $elapsedTime.Days, $elapsedTime.Hours, $elapsedTime.Minutes, $elapsedTime.Seconds
+                                HotFix=$null
+                                HFInstallDate=$null
+                            }
+                        }
+                    } -Name "Uptime Information" -ArgumentList $Computer, $ComputerName, $KB
+                }
+
+                else {
+
+                    Start-Job {param ($Computer)
+
+                        [pscustomobject] @{
+
+                            ComputerName = $Computer
+                            LastBootTime = 'Ping failed'
+                            ElapsedTime = 'N/A'
+                            HotFix='N/A'
+                            HFInstallDate=$null
+                        }
+                    } -Name "PING Failed" -ArgumentList $Computer
+                }
+            }
+        }
+    } 
+
+    if(!($FindHotFix.IsPresent)) {
+
+        UptimeReport | Receive-Job -Wait -AutoRemoveJob |  Sort ComputerName | Select ComputerName, LastBootTime, ElapsedTime | Out-GridView -Title "System Uptime" #| Export-Csv $OutFile -NoTypeInformation -Force
+        #Write-Host -ForegroundColor Green "Server uptime results saved to $OutFile"
+    }
+
+    else {
+
+        UptimeReport | Receive-Job -Wait -AutoRemoveJob |  Sort ComputerName | Select ComputerName, LastBootTime, ElapsedTime, HotFix, HFInstallDate | Out-GridView -Title "System Uptime and KB Information" #| Export-Csv $OutFile -NoTypeInformation -Force
+        #Write-Host -ForegroundColor Green "Server uptime results saved to $OutFile"
+    }
+}#End PatchSearch
 
 function LoggedUser{
   <# 
@@ -2307,3 +2469,5 @@ $Stamp = (Get-Date -Format G) + ":"
 #Show Form
 $Form.ShowDialog() | out-null
 }#EndGUI
+
+ 
